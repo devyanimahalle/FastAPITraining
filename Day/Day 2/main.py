9356f81c73ce124a1cc3f414aac9aa8cb160c1cc
Devyani Mahalle <devyanism1207@gmail.com>
2026-09-22 12:54:40 +0530

    Adding multiple roots

--- Day/Day 2/main.py
+++ Day/Day 2/main.py
@@ -2,4 +2,13 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return {"message":"Hello World"}
+    return {"message": "Hello World"}
+def home():
+    return {"page":"Home"}
+
+@app.get("/about")
+def about():
+    return {"page":"About","author":"Rakesh"}
+@app.get("/health")
+def health():
+    return {"satus":"ok"}
